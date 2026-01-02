0002015c940de690a4dd00707263881e91dba84d
Kate Mason <143445392+kmaso99@users.noreply.github.com>
2026-01-02 09:57:49 -0800

    Update plotting.py

--- utils/plotting.py
+++ utils/plotting.py
@@ -5,20 +5,6 @@ Provides visualization utilities for diagnostics, performance charts,
 and attribution plots.
 """
 
-# remove this line
-def plot_series(series, title: str = ""):
-    """
-    Plot a time series.
-
-    Parameters:
-        series (pd.Series): Data to plot.
-        title (str): Chart title.
-
-    Returns:
-        None
-    """
-    pass
-
 
 def plot_series(series, title: str = ""):
     """
