84e71bccc78fd62553f49310b6dcc5cdf8fde847
Kate Mason <143445392+kmaso99@users.noreply.github.com>
2026-01-01 17:18:15 -0800

    Add docstring to plot_series function
    Added docstring for the plot_series function.

--- utils/plotting.py
+++ utils/plotting.py
@@ -1,1 +1,23 @@
+"""
+plotting.py
+
+Provides visualization utilities for diagnostics, performance charts,
+and attribution plots.
+"""
+
+import matplotlib.pyplot as plt
+
+
+def plot_series(series, title: str = ""):
+    """
+    Plot a time series.
+
+    Parameters:
+        series (pd.Series): Data to plot.
+        title (str): Chart title.
+
+    Returns:
+        None
+    """
+    pass
 
